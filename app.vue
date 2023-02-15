92f532e5b6c8e79f755fcfb9d7313612df0859ce
OKEREAFOR ERI-NGOZI <erreakezy@gmail.com>
2023-02-15 10:09:12 +0100

    ADD: home component

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Home />
   </div>
 </template>
+
+<script>
+import Home from '~/components/home/Home';
+
+export default {
+  components: {
+    Home,
+  },
+  
+};
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+//import { defineNuxtConfig } from "nuxt3";
+
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   postcss: {
@@ -7,4 +10,19 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/variables.scss" as *;', // contains just variables/mixins/functions needed for components and global styles ( do not include any compilable css)
+        },
+      },
+    },
+  },
+  // styleResources: {
+  //   scss: ["~/assets/style/variables.scss"],
+  // },
+
+  // // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // buildModules: ["@nuxtjs/style-resources"],
 });
